739dc2e79dab10a1ae590c8e7b53b43e43800cdd
Hiba <hibakaawan98@gmail.com>
2024-12-15 21:43:55 -0600

    added env file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 import photosRoutes from "./routes/photos.js";
 import tagsRoutes from "./routes/tags.js";
 import cors from "cors";
+import "dotenv/config";
 
-const PORT = 8080;
+//setting server port value found .env
+const PORT = process.env.PORT || 7070;
 //midleware
 app.use(cors());
 app.use(express.json());
@@ -19,5 +21,5 @@ app.get("/", (_req, res)=>{
 });
 
 app.listen(PORT, ()=>{
-    console.log(`Server running at http://localhost:${PORT}`)
+    console.log(`Server running at ${process.env.BACKEND_URL}${PORT}`);
 });
